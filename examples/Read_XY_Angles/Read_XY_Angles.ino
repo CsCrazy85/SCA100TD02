80addf1256e1f4bf78a5a1c88f5781d08696fdc5
Mika Väyrynen <mika_va@hotmail.com>
2020-09-11 17:21:30 +0300

    Added some commentary

--- examples/Read_XY_Angles/Read_XY_Angles.ino
+++ examples/Read_XY_Angles/Read_XY_Angles.ino
@@ -1,3 +1,13 @@
+/*
+    Basic Example 1.
+
+    SCA100T -library allows communication with Murata SCA100T inclinometer sensors using SPI.
+
+    This sketch shows the basic usage of the sensor interface. 
+    Program reads the angle from the sensor and prints it to a serial port.
+  
+*/
+
 #include <SCA100T.h>
 int chipSelect = 10;
 long spiBusSpeed = 500000;
@@ -13,7 +23,7 @@ void setup()
 
 void loop() 
 {
-  float angle_X = mySensor.getAngle(SCA_ACCEL_Y);
+  float angle_X = mySensor.getAngle(SCA_ACCEL_X);
   float angle_Y = mySensor.getAngle(SCA_ACCEL_Y);
   Serial.print(F("Angle X: "));
   Serial.print(angle_X);
